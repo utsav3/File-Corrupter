a9c6d47b66d327546165b9a311881d4483095422
Ayush Kohli <kohli.ayush@gmail.com>
2016-04-09 16:56:09 -0500

    Currently only prints to console and there is an infinite loop bug which has to be fixed

---
+++ corrupt.asm
@@ -0,0 +1,56 @@
+;Written by Ayush Kohli and Utsav Dhungel
+
+section .bss
+  buffer : resb 1
+
+section .data
+SYS_EXIT equ 1
+SYS_WRITE equ 4
+SYS_READ equ 3
+READWRITE_MODE equ 2
+;STDIN equ 0
+;STDOUT equ 1
+SYS_OPEN equ 5
+KERNAL equ 0x80
+
+section .text
+  global _start
+
+  exit:
+   mov eax, SYS_EXIT
+   mov ebx, 0 ; EXIT STATUS FOR SUCESS
+   int KERNAL
+
+  _start:
+   pop ebx
+   pop ebx
+   pop ebx
+  ; call open
+  jmp open
+
+
+  open:
+   mov eax, SYS_OPEN
+   mov ecx, READWRITE_MODE
+   int KERNAL
+   test eax,eax
+   jns read
+  ; ret
+
+  read:
+   mov ebx, 3
+   mov eax, SYS_READ
+   mov ecx, buffer
+   mov edx, 1
+   int KERNAL
+   jmp write
+
+  write:
+   mov ebx, 1
+   mov eax, SYS_WRITE
+   mov ecx, buffer
+   mov edx, 1
+   int KERNAL
+   jmp read
+
+  ;rand:
